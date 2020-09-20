8c38f9ddf7d773f409f0673271f7174cba0308fb
natswatch <nathaliec@me.com>
2020-09-20 12:41:14 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const inquirer = require('inquirer');
+
+// title of my project and sections entitled Description, Table of Contents, 
+//Installation, Usage, License, Contributing, Tests, and Questions
+
+//ections of the README entitled Description, Installation, Usage, 
+//Contributing, and Tests
+
+//choose a license for my application from a list of options
+//a badge for that license is added near the top of the README 
+//explains which license the application is covered under
+
+//I enter my GitHub username
+//added to the section of the README entitled Questions, 
+//with a link to my GitHub profile
+
+//my email address
+// added to the section of the README entitled Questions, 
+//with instructions on how to reach me with additional 
+//questions
+
+// WHEN I click on the links in the Table of Contents
+//taken to the corresponding section of the README
+
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
+
