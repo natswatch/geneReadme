b6fb23610dcb567edbb8d404b8568a8574118070
natswatch <nathaliec@me.com>
2020-09-20 16:18:50 -0400

    inquirer prompts

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 // title of my project and sections entitled Description, Table of Contents, 
 //Installation, Usage, License, Contributing, Tests, and Questions
@@ -23,12 +24,94 @@ const inquirer = require('inquirer');
 //taken to the corresponding section of the README
 
 // array of questions for user
-const questions = [
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project? (required)',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a title');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Include a description (required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Provide installation instructions:'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide usage information:'
+        },
+        {
+            type: 'checkbox',
+            name: 'licenses',
+            message: 'Which license(s) is this application covered under (Check all that apply)',
+            choices: ['MIT','ISC', 'GNU GPLv2', 'Apache License 2.0']
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Provide info on ways to contribute:'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Tests for this project:'
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: 'What is your Github username? (required)',
+            validate: usernameInput => {
+                if (usernameInput) {
+                    return true;
+                } else {
+                    console.log('Please provide your Github username')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address? (required)',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please provide your email address')
+                    return false;
+                }
+            }
+        }
+        
+    ]);
 
-];
+};
 
 // function to write README file
-function writeToFile(fileName, data) {
+function writeToFile = (fileName, data) => {
+    
 }
 
 // function to initialize program
